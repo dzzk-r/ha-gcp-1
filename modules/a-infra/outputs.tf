00692bb551d84ccfe9387acd3c30c8360331eb61
Daniel Chechik <danielworkload@gmail.com>
2025-02-18 14:25:13 +0200

    Ingress && LB output-values added

--- modules/a-infra/outputs.tf
+++ modules/a-infra/outputs.tf
@@ -14,3 +14,16 @@ output "cloud_armor_policy" {
   description = "Cloud Armor policy applied"
   value       = google_compute_security_policy
 }
+
+output "lb_external_ip" {
+  description = "External IP адрес LoadBalancer"
+  value       = google_compute_global_address.external_lb.address
+}
+
+output "ingress_status" {
+  description = "Проверка доступности Ingress"
+  value       = <<EOT
+  Запустите:
+  curl -i http://$(kubectl get ingress flask-ingress -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+  EOT
+}
