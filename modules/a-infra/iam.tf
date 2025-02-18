3699e0e14a82c4deea67a3bfcf863e72aae51aaa
Daniel Chechik <danielworkload@gmail.com>
2025-02-18 14:23:18 +0200

    GCP Load Balancer check via IAM added

--- modules/a-infra/iam.tf
+++ modules/a-infra/iam.tf
@@ -1,1 +1,7 @@
-resource "google_project_iam_member" "gke_restrict_sa" {}
+# resource "google_project_iam_member" "gke_restrict_sa" {}
+
+resource "null_resource" "check_iam" {
+  provisioner "local-exec" {
+    command = "gcloud compute forwarding-rules list || echo 'No access to GCP Load Balancer!'"
+  }
+}
